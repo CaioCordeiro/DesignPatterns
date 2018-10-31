cc18a2c5b944752006e62275d6059666a8318b2f
Caio Cordeiro <ccmr@cesar.school>
2018-10-30 21:57:57 -0300

    Almost Done

---
+++ Adapter/adapter.py
@@ -0,0 +1,28 @@
+def adap(nome_arquivo):
+    agenda = [] 
+    agenda2 = []## Armazenara os números finais
+    arquivo = open(nome_arquivo,"r") 
+
+    ## Navegando pelo arquivo e salvando as listas em um array
+    for linha in arquivo: 
+        numeros = arquivo.readlines()
+
+    ## Navegando pelo array e retirando os caracteres não essenciais
+    tamanho = len(numeros)
+    for i in numeros: 
+        for n in i:
+            if n == '(' or n == ')' or n == ' ' or n == '-' or n == '\n':
+                continue
+            else:
+                agenda.append(n)
+        agenda = ''.join(agenda)
+        agenda2.append(agenda)
+        agenda=[]
+
+    return agenda2
+    arquivo.close()
+
+nome_arquivo = "numero.txt"
+agenda2 = adap(nome_arquivo)
+for i in agenda2:
+    print(i)
---
+++ Composite/Composite.py
@@ -0,0 +1,24 @@
+class animals():
+    class aves():
+        class galinha():
+            tipo = "Isso é uma galinha"
+        class pombo():
+            tipo = "Isso é um pombo"
+        class pato():
+            tipo = "Isso é um pato"
+    class mamiferos():
+        class cachorro():
+            tipo = "Isso é um cachorro"
+        class golfinho():
+            tipo = "Isso é um golfinho"
+        class onitorrinco():
+            tipo = "Isso é um golfinho"
+class pastas():
+    class Series:
+        class Doctor_Who:
+            class temp1:
+                print("Episodios 1 -19")
+        class Demolidor:
+            class temp1:
+                print("Episodios 1 -19")
+                
